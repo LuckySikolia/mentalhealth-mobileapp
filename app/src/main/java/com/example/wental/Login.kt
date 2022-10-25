93a989b4e06ce4220813858b67f9c315c80396d8
Lucky <luckysikolia7@gmail.com>
2022-10-26 01:02:29 +0300

    Updated layout by -Changing all caps default to sentence case on buttons -Setting up firebase authentication with email and password

--- app/src/main/java/com/example/wental/Login.kt
+++ app/src/main/java/com/example/wental/Login.kt
@@ -1,11 +1,49 @@
 package com.example.wental
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.wental.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.`activity_login.xml`)
+
+        //view binding
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //if user doesn't have an account redirect to registration
+        binding.loginPageRedirect.setOnClickListener {
+            val intent = Intent(this, Registration::class.java)
+            startActivity(intent)
+        }
+
+        //button action
+        binding.loginContainedButton.setOnClickListener{
+            val email = binding.EmailET.toString()
+            val pass = binding.PasswordET.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener{
+                    if (it.isSuccessful){
+
+                        //send user to main activity ie home-screen
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Empty fields are not allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/wental/Registration.kt
+++ app/src/main/java/com/example/wental/Registration.kt
@@ -1,12 +1,55 @@
 package com.example.wental
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.wental.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Registration : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrationBinding
+
+    //create firebase authentication object
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+
+        //view binding
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //initializing auth object
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //if user is registered redirect to login page
+        binding.regPageRedirect.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+        //button action
+        binding.registerContainedButton.setOnClickListener {
+            //might have to remove the username feature if work around is not found
+           val username = binding.UsernameET.text.toString()
+            val email = binding.EmailET.text.toString()
+            val pass = binding.PasswordET.text.toString()
+
+            if (username.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        //send user to login activity
+                        val intent = Intent(this, Login::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty fields are not allowed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
